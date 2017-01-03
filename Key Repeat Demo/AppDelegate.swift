be6365b434d37171340d1634c26e2bc0d48c650f
Barry Allard <barry.allard@gmail.com>
2017-01-03 13:22:34 -0800

    Initial Commit

---
+++ Key Repeat Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Key Repeat Demo
+//
+//  Created by Bad Motherfucker on 1/3/17.
+//  Copyright © 2017 BMF. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
